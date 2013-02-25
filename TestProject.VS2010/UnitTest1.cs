cd13cbab619cc1fe8cd9c25a63711ed125a3bfa4
Abhishek Gopalan <abhishek_gopalan@external.mckinsey.com>
2013-02-25 19:21:57 +0530

    Added tasks for external tools invocation

---
+++ TestProject.VS2010/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject.VS2010
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.IsTrue(true);
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			Assert.IsTrue(false);
+		}
+	}
+}
